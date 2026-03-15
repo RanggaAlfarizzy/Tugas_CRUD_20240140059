9f8bab85426a468b9eb49d5c8b8ccd75c93665bf
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2026-03-16 00:53:55 +0800

    menambahkan getter dan setter pada entity Ktp

--- src/main/java/com/example/ktp/entity/Ktp.java
+++ src/main/java/com/example/ktp/entity/Ktp.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="ktp")
+@Table(name = "ktp")
 public class Ktp {
 
     @Id
@@ -15,7 +15,6 @@ public class Ktp {
     private String nomorKtp;
 
     private String namaLengkap;
-
     private String alamat;
 
     @Temporal(TemporalType.DATE)
@@ -24,4 +23,51 @@ public class Ktp {
     private String jenisKelamin;
 
     // getter setter
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomorKtp() {
+        return nomorKtp;
+    }
+
+    public void setNomorKtp(String nomorKtp) {
+        this.nomorKtp = nomorKtp;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
 }
